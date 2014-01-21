78269fdc64fce2802affef99bbc145709351d530
guinaudin <guillaume.naudin78@gmail.com>
2014-01-21 17:19:54 +0100

    Modifying web.xml + RecommendationService

--- src/main/java/fr/ece/recommendation/RecommendationService.java
+++ src/main/java/fr/ece/recommendation/RecommendationService.java
@@ -4,25 +4,25 @@ import com.google.gson.Gson;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/WebService")
-public class RecommendationService {
+public class RecommendationService extends HttpServlet {
     @GET
-    @Path("/getArtitsRecommendation")
-    @Produces("application/json")
-    public void printJsonRecommendation() {
+    @Produces(MediaType.APPLICATION_JSON)
+    public String printJsonRecommendation() {
+        String gsonRecommendation = null;
         try {
             ArtistsRecommendationProcess artistsRecommendationProcess = new ArtistsRecommendationProcess();
             
             //TO DO modify userID
             ArtistsRecommendation artistsRecommendation = artistsRecommendationProcess.getArtitsRecommendation(1);
-            Gson gson= new Gson();
-            gson.toJson(artistsRecommendation);
-            
-            System.out.println(gson.toJson(artistsRecommendation));
+            Gson gson = new Gson();
+            gsonRecommendation = gson.toJson(artistsRecommendation);
         } 
         catch (SQLException ex) {
             Logger.getLogger(RecommendationService.class.getName()).log(Level.SEVERE, null, ex);
@@ -30,5 +30,7 @@ public class RecommendationService {
         catch (ClassNotFoundException ex) {
             Logger.getLogger(RecommendationService.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        return gsonRecommendation;
     }    
 }
