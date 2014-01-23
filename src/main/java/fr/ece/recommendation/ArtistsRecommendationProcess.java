0b48d3a0117c65631d799796e41d07c73ca0c9a2
guithieb <thiebaut.guillaume@gmail.com>
2014-01-23 16:33:24 +0100

    Verification resultset different null

--- src/main/java/fr/ece/recommendation/ArtistsRecommendationProcess.java
+++ src/main/java/fr/ece/recommendation/ArtistsRecommendationProcess.java
@@ -11,29 +11,29 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ArtistsRecommendationProcess {
+
     private final PreparedStatement selectArtistPreparedStatement;
     private final PreparedStatement selectArtistIdListPreparedStatement;
-    private  Connection myCon;
+    private Connection myCon;
     private ResultSet resultSet;
-    
+
     public ArtistsRecommendationProcess() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         myCon = DriverManager.getConnection("jdbc:mysql://ec2-50-19-213-178.compute-1.amazonaws.com:3306/zappprofile", "guinaudin", "zappTeam");
         //Pas d'auto commit
         myCon.setAutoCommit(false);
-        
+
         selectArtistPreparedStatement = myCon.prepareStatement("SELECT firstName, familyName FROM Artists WHERE artistId = ?");
         selectArtistIdListPreparedStatement = myCon.prepareStatement("SELECT artistIdList FROM ArtistsRecommendations WHERE userId = ?");
     }
-    
+
     public ArtistsRecommendation getArtitsRecommendation(int userId) throws SQLException {
-       
-        
+
+
         ArtistsRecommendation artistsRecommendation = null;
         List<Artist> artistList = new ArrayList<Artist>();
-        
-        if (myCon.isClosed())
-        {
+
+        if (myCon.isClosed()) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
             } catch (ClassNotFoundException ex) {
@@ -41,27 +41,30 @@ public class ArtistsRecommendationProcess {
             }
             myCon = DriverManager.getConnection("jdbc:mysql://ec2-50-19-213-178.compute-1.amazonaws.com:3306/zappprofile", "guinaudin", "zappTeam");
         }
-        
+
         selectArtistIdListPreparedStatement.setLong(1, userId);
         resultSet = selectArtistIdListPreparedStatement.executeQuery();
         myCon.commit();
-        
-        resultSet.next();
-        String temp = resultSet.getString(1);
-        String[]artistTab = temp.split(",");
 
-        if(artistTab.length > 0) {
-            for(int i = 0; i < artistTab.length; i++) {
-                selectArtistPreparedStatement.setLong(1, Long.parseLong(artistTab[i]));
-                resultSet = selectArtistPreparedStatement.executeQuery();
-                myCon.commit();
-                resultSet.next();
-                
-                artistList.add(new Artist(resultSet.getString(1), resultSet.getString(2)));
+
+        if (resultSet.next()) {
+            String temp = resultSet.getString(1);
+            String[] artistTab = temp.split(",");
+
+            if (artistTab.length > 0) {
+                for (int i = 0; i < artistTab.length; i++) {
+                    selectArtistPreparedStatement.setLong(1, Long.parseLong(artistTab[i]));
+                    resultSet = selectArtistPreparedStatement.executeQuery();
+                    myCon.commit();
+
+                    if (resultSet.next()) {
+                        artistList.add(new Artist(resultSet.getString(1), resultSet.getString(2)));
+                    }
+                }
+                artistsRecommendation = new ArtistsRecommendation(userId, artistList);
             }
-            artistsRecommendation = new ArtistsRecommendation(userId, artistList);
+
         }
-        
         myCon.close();
 
         return artistsRecommendation;
