008dc2fc86e644f0b8d2cca368a9862550c3ce09
guinaudin <guillaume.naudin78@gmail.com>
2014-01-24 10:05:46 +0100

    no message

--- src/main/java/fr/ece/historic/ApplicationStartup.java
+++ src/main/java/fr/ece/historic/ApplicationStartup.java
@@ -31,7 +31,7 @@ public class ApplicationStartup implements ServletContextListener{
             CronTrigger cronTrigger = new CronTrigger("cronTrigger", "triggerGroup2");
             try {
                 // setup CronExpression
-                CronExpression cexp = new CronExpression("0 25 5 ? * THU");
+                CronExpression cexp = new CronExpression("0 20 10 ? * FRI");
                 // Assign the CronExpression to CronTrigger
                 cronTrigger.setCronExpression(cexp);
             } catch (Exception e) {
