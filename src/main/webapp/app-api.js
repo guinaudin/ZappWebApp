19f2154cec2520cbc762c633fd145d74cc535baa
guithieb <thiebaut.guillaume@gmail.com>
2014-01-27 10:15:12 +0100

    Ajout Librairie JS

--- src/main/webapp/app-api.js
+++ src/main/webapp/app-api.js
@@ -1,19 +1,3 @@
-/* 
- * Copyright 2014 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /*
  * /////////////////////////////////////////
  *      BBox Lab API - 2013 - V0.0.2
@@ -594,5 +578,3 @@
     window.APP = APP; // Global APP obj
 
 })(window);
-
-
